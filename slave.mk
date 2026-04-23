dc1c8f849a698ebab583ad7a75eba176cbae11d3
mssonicbld <79238446+mssonicbld@users.noreply.github.com>
2026-04-24 04:13:31 +0800

    [slave.mk]: Add linux-kbuild install dependency to installer target (#26955)
    #### Why I did it
    
    Fix https://github.com/sonic-net/sonic-buildimage/issues/26483
    
    Currently, the `linux-kbuild` package (which provides the `sign-file` utility) is only installed into the sonic-slave container during the `rfs.squashfs` build step in `build_debian.sh`.
    
    On incremental rebuilds, the `rfs.squashfs` target is not rebuilt, so `linux-kbuild` is never re-installed into the container, causing the signing step to fail with `ERROR: LOCAL_SIGN_FILE=... file does not exist`.
    
    #### How I did it
    * Added `$(LINUX_KBUILD)-install` as a dependency to the `$(SONIC_INSTALLERS)` target in `slave.mk`
    * This ensures the `linux-kbuild` package is installed into the sonic-slave container before `.bin` generation, regardless of whether the `rfs.squashfs` target was rebuilt
    * This approach was suggested by @saiarcot895 as the preferred fix, since it records the dependency explicitly in make rather than relying on a side effect of the squashfs build step
    
    #### How to verify it
    1. Build a signed image: `make ENABLE_ZTP=y all`
    2. Move the output: `mv target/sonic.bin target/sonic.ztp.bin`
    3. Build again with different flags: `make ENABLE_ZTP=n all`
    4. Verify the second build completes without `sign-file` errors
    
    #### Description for the changelog
    * Fixed incremental rebuild failure for signed images by adding linux-kbuild install dependency to the installer target
    
    Signed-off-by: Sonic Build Admin <sonicbld@microsoft.com>

--- slave.mk
+++ slave.mk
@@ -1497,7 +1497,8 @@ $(addprefix $(TARGET_PATH)/, $(SONIC_INSTALLERS)) : $(TARGET_PATH)/% : \
         $(addprefix $(PYTHON_WHEELS_PATH)/,$(SONIC_YANG_MGMT_PY3)) \
         $(addprefix $(PYTHON_WHEELS_PATH)/,$(SYSTEM_HEALTH)) \
         $(addprefix $(PYTHON_WHEELS_PATH)/,$(SONIC_HOST_SERVICES_PY3)) \
-        $$(addprefix $(TARGET_PATH)/,$$($$*_RFS_DEPENDS))
+        $$(addprefix $(TARGET_PATH)/,$$($$*_RFS_DEPENDS)) \
+        $(addprefix $(IMAGE_DISTRO_DEBS_PATH)/,$(LINUX_KBUILD)-install)
 
 	$(HEADER)
 	# Pass initramfs and linux kernel explicitly. They are used for all platforms
